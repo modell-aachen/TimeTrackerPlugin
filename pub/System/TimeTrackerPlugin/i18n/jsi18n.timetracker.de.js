6325a148f27d48c0388c7b14a65f8d8bded8d693
Aaron Grabowy <grabowy@modell-aachen.de>
2016-09-22 13:29:47 +0200

    Added german localization

---
+++ pub/System/TimeTrackerPlugin/i18n/jsi18n.timetracker.de.js
@@ -0,0 +1,26 @@
+jsi18n.add('timetracker', {
+    "Add activity": "Aktivit&auml;t hinzuf&uuml;gen",
+    "Book": "Buchen",
+    "Book in Redmine": "In Redmine buchen",
+    "Booked in Redmine": "In Redmine gebucht",
+    "Cancel edit": "Abbrechen",
+    "Comment": "Kommentar",
+    "Duration": "Dauer",
+    "Delete activity": "Aktivit&auml;t l&ouml;schen",
+    "From": "Von",
+    "Including comment": "Einschlie&szlig;lich Kommentar",
+    "Project": "Projekt",
+    "Run": "Start",
+    "Save edit": "Speichern",
+    "Send comment": "Kommentar mitsenden",
+    "Status": "Status",
+    "Ticket": "Ticket",
+    "Todays total time": "Heutige Gesamtzeit",
+    "Total time": "Gesamtzeit",
+    "To": "Bis",
+    "Type": "Typ",
+    "Unbook": "R&uuml;ckbuchen",
+
+    "Something went wrong during saving the data in the topics meta data. Please have a look at the red marked activities and redo any edits/deletions. Alternatively you can reload the page to discard every unsaved changes and display the currently saved status.": "Etwas ist beim Speichern der Daten in den Metadaten schief gelaufen. Schauen Sie sich bitte die rot markierten Aktivit&auml;ten an und wiederholen Sie deren Ver&auml;nderung. Alternativ k&ouml;nnen Sie die Seite neu laden, um ungespeicherte &Auml;nderungen zu verwerfen und den aktuell gespeicherten Stand zu laden.",
+    "The server refused to save the data. Please check if your date and time is correctly set and try again. Red marked activities contain unsaved data.": "Der Server hat das Speichern abgelehnt. Bitte &uuml;berpr&uuml;fen Sie, ob Datum und Uhrzeit korrekt eingestellt sind. Rot markierte Aktivit&auml;ten enthalten ungespeicherte Daten."
+});
